bb6d3862ddbc25b0e9e668dbdb4da51e2b82822b
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-02-21 20:08:26 +0530

    update

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -45,7 +45,7 @@ function Chatpage (){
 
     const socket = useMemo(
       () =>
-        io("https://projectyle-chat-backend.vercel.app", {
+        io("http://localhost:8080", {
           withCredentials: false,
           
           
